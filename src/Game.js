5efc5e7e5338bc6c49910a09531477b8a4b62487
magdakok <61601293+magdakok@users.noreply.github.com>
2020-07-17 21:58:47 +0100

    style initially Header

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
+import { scissors, paper, rock, lizard, spock } from "./rules";
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: radial-gradient(
+    circle at top,
+    var(--gradient-lighter),
+    var(--gradient-darker)
+  );
+  padding: 50px 25%;
+`;
 
 function Game() {
-  return <div className='Game'>GAME</div>;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 }
 
 export default Game;

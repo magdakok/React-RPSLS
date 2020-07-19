d7fde4226537ff1587432fa9e8d23830dde4c316
magdakok <61601293+magdakok@users.noreply.github.com>
2020-07-19 20:39:54 +0100

    Updates

---
+++ precache-manifest.18cdde3aee75fdbfb1b6bfa819a52eee.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d88f9a4c195e2c9b6f6c898821cd91",
+    "url": "/React-RPSLS/index.html"
+  },
+  {
+    "revision": "0d713242c308d6a1fe2a",
+    "url": "/React-RPSLS/static/css/main.3b4aeec4.chunk.css"
+  },
+  {
+    "revision": "94a97a4f2f51ef4d3bb8",
+    "url": "/React-RPSLS/static/js/2.2ebead64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-RPSLS/static/js/2.2ebead64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d713242c308d6a1fe2a",
+    "url": "/React-RPSLS/static/js/main.0017a7a1.chunk.js"
+  },
+  {
+    "revision": "523e8722f8735f4bcb5b",
+    "url": "/React-RPSLS/static/js/runtime-main.15f5a73d.js"
+  },
+  {
+    "revision": "aced70522265bc609155bea44802776b",
+    "url": "/React-RPSLS/static/media/bg-pentagon.aced7052.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/React-RPSLS/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "7d2e0b18d2860b90407f41297dce461f",
+    "url": "/React-RPSLS/static/media/icon-lizard.7d2e0b18.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/React-RPSLS/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/React-RPSLS/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/React-RPSLS/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "b6446e8e1e5d5609deb209d401e779a3",
+    "url": "/React-RPSLS/static/media/icon-spock.b6446e8e.svg"
+  },
+  {
+    "revision": "bab8b554708a6ff7bb93a233f2ff7cf2",
+    "url": "/React-RPSLS/static/media/image-rules-bonus.bab8b554.svg"
+  },
+  {
+    "revision": "f4c973f743c8b75de9da23335e126dcd",
+    "url": "/React-RPSLS/static/media/logo-bonus.f4c973f7.svg"
+  }
+]);
